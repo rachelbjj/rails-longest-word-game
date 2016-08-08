ab9dbecd6a8343903352928465a8184816df8c99
rachel BJORK <rachel.bjork@edhec.com>
2016-08-08 18:11:10 +0200

    rails new

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,25 +1,51 @@
 class GameController < ApplicationController
 
   def prompt
+    @play_grid = generate_grid(9)
+    @start_time = Time.now
   end
 
   def result
+    @end_time = Time.now
+    @start_time = params[:time]
+    @word = params[:query]
+    run_game(@word, @start_time, @end_time)
   end
 
   private
+    def run_game(attempt, start_time, end_time)
+
+
+    response = open("http://api.wordreference.com/0.8/80143/json/enfr/#{attempt.downcase}")
+    json = JSON.parse(response.read.to_s)
+
+
+      score = 0
+      message= []
+
+      if attempt.size > 2
+        score =+ 3
+      else
+        result[:score] += 2
+      end
+
+      if score > 3
+        message ="well done !"
+      else
+        message = "Too bad."
+      end
+
+    return result = {
+        time: end_time-start_time,
+        score: score,
+        message: message
+      }
+
+    end
+
 
     def generate_grid(grid_size)
-    @play_grid = ?
-    grid_size.to_i
-    grid = (1..grid_size).to_a
-    letters = ("A".."Z")
-
-    grid = grid.map do
-      ("A".."Z").to_a.sample(1)
+      grid = Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
     end
-    return grid
-
-    @play_grid = generate_grid(9)
-  end
 
 end
